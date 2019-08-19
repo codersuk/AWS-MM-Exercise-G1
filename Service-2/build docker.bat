01e6bd2d7f287e85c0951598d7b9d3a09e0ac50f
Alessandro <alessandro.piccione.75@gmail.com>
2019-08-20 00:48:55 +0100

    Service-2: add Dockerfile. add logging.

---
+++ Service-2/build docker.bat
@@ -0,0 +1,5 @@
+:: docker build <new-image-name:tag> <folder where the Dockerfile is>
+
+docker build -t flaskapp:latest . 
+
+pause
--- Service-2/src/app.py
+++ Service-2/src/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, Response
+import logging as logger
+
+logger.basicConfig(level="INFO")
 
 app = Flask("Service Two")
 
 @app.route("/random")
 def random():
     import random
-    return Response(f'{{number: { int(1000 * random.random()) }}}', mimetype="application/json")
+    data = f'{{"number": {int(1000 * random.random())} }}'
+    return Response( data, content_type="application/json")
 
-app.run()
+logger.info("Start Flask server")
+app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
